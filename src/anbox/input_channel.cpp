4d40065a488d20b90ba4ae941ec515f8f45b7192
Simon Fels <morphis@gravedo.de>
2016-06-20 09:47:50 +0200

    Drop libevdev usage

--- src/anbox/input_channel.cpp
+++ src/anbox/input_channel.cpp
@@ -22,54 +22,21 @@
 
 #include <stdexcept>
 
-#include <libevdev/libevdev.h>
-#include <libevdev/libevdev-uinput.h>
-
 namespace anbox {
 InputChannel::InputChannel() :
     dev_(nullptr) {
 }
 
 InputChannel::~InputChannel() {
-    if (dev_)
-        libevdev_uinput_destroy(dev_);
 }
 
 void InputChannel::setup(int width, int height) {
-    DEBUG("");
-    auto dev = libevdev_new();
-    libevdev_set_name(dev, "anbox-tp");
-
-    libevdev_enable_event_type(dev, EV_ABS);
-    struct input_absinfo mt_tracking_id = { 0, 0, 255, 0, 0, 0 };
-    libevdev_enable_event_code(dev, EV_ABS, ABS_MT_TRACKING_ID, &mt_tracking_id);
-    struct input_absinfo mt_slot = { 0, 0, 255, 0, 0, 0 };
-    libevdev_enable_event_code(dev, EV_ABS, ABS_MT_SLOT, &mt_slot);
-    struct input_absinfo mt_pos_x = { 0, 0, width, 0, 0, 0 };
-    libevdev_enable_event_code(dev, EV_ABS, ABS_MT_POSITION_X, &mt_pos_x);
-    struct input_absinfo mt_pos_y = { 0, 0, height, 0, 0, 0 };
-    libevdev_enable_event_code(dev, EV_ABS, ABS_MT_POSITION_Y, &mt_pos_y);
-    struct input_absinfo mt_pressure = { 0, 0, 5, 0, 0, 0 };
-    libevdev_enable_event_code(dev, EV_ABS, ABS_MT_PRESSURE, &mt_pressure);
-
-    libevdev_enable_event_type(dev, EV_SYN);
-
-    libevdev_enable_property(dev, INPUT_PROP_DIRECT);
-
-    if (libevdev_uinput_create_from_device(dev, LIBEVDEV_UINPUT_OPEN_MANAGED, &dev_) < 0) {
-        libevdev_free(dev);
-        BOOST_THROW_EXCEPTION(std::runtime_error("Failed to create uinput based device"));
-    }
 }
 
 void InputChannel::push_event(const Event &event) {
-    libevdev_uinput_write_event(dev_, event.type, event.code, event.value);
 }
 
 std::string InputChannel::dev_path() const {
-    if (!dev_)
-        return "";
-
-    return std::string(libevdev_uinput_get_devnode(dev_));
+    return "";
 }
 } // namespace anbox
