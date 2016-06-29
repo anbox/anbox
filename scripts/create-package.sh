3f6445215884d22e6371e318181255ae1ed2820c
Simon Fels <morphis@gravedo.de>
2016-06-30 00:10:12 +0200

    scripts: tweak rootfs package creation a bit

--- scripts/create-package.sh
+++ scripts/create-package.sh
@@ -15,6 +15,14 @@ cp -r $OUTDIR/system/* rootfs/system/
 mkdir rootfs/cache
 
 find out -name filesystem_config.txt -exec cp {} rootfs \;
+if [ ! -e rootfs/filesystem_config.txt ] ; then
+	echo "ERROR: Filesystem config is not available. You have to run"
+	echo "ERROR: $ make target-files-package"
+	echo "ERROR: to generate it as part of the Android build."
+	rm -rf rootfs
+	exit 1
+fi
+
 cp anbox-init.sh rootfs/
 chmod +x rootfs/anbox-init.sh
 
@@ -24,3 +32,4 @@ chmod 755 rootfs/system/build.prop
 chmod +x rootfs/anbox-init.sh
 
 tar cf rootfs.tar rootfs
+rm -rf rootfs
