2cbc3a5021a426abad517e3c2cf8a655f9f849de
Simon Fels <morphis@gravedo.de>
2016-11-18 12:54:20 +0100

    Drop all properties which are now in build.prop

--- src/anbox/qemu/boot_properties_message_processor.cpp
+++ src/anbox/qemu/boot_properties_message_processor.cpp
@@ -28,7 +28,6 @@ BootPropertiesMessageProcessor::BootPropertiesMessageProcessor(const std::shared
 BootPropertiesMessageProcessor::~BootPropertiesMessageProcessor() {
 }
 
-
 void BootPropertiesMessageProcessor::handle_command(const std::string &command) {
     if (command == "list")
         list_properties();
@@ -36,42 +35,9 @@ void BootPropertiesMessageProcessor::handle_command(const std::string &command) 
 
 void BootPropertiesMessageProcessor::list_properties() {
     std::vector<std::string> properties = {
-        // Simple indicator to say we're anbox and if needed things in the container
-        // can adjust to this. Normally everything should detect we're qemu which
-        // should be enough for the simple cases.
-        "ro.anbox=1",
-
-        // Needed for identification within the system. Normally retrieve from
-        // kernel configuration which isn't possible in our case.
-        "ro.hardware=goldfish",
-
-        // Needed to let the gralloc HAL load the right implementation
-        "ro.kernel.qemu.gles=1",
-
-        // Needed from different parts of the system in order to load
-        // the right implementation for qemu
-        "ro.kernel.qemu=1",
-
         // TODO(morphis): Using HDPI here for now but should be adjusted to the device
         // we're running on.
         utils::string_format("ro.sf.lcd_density=%d", static_cast<int>(graphics::DensityType::medium)),
-
-        // Disable on-screen virtual keys as we can use the hardware keyboard
-        "qemu.hw.mainkeys=1",
-
-        // Android has builtin detection (inside Zygote) for proper container
-        // detection support
-        "ro.boot.container=1",
-
-        // Mark us as a device without telephony support (as we don't have a RIL)
-        "ro.radio.noril=yes",
-
-        // To let surfaceflinger load our hwcomposer implementation we specify
-        // the correct subkey of the module here.
-        "ro.hardware.hwcomposer=anbox",
-
-        // Keep boot animation disabled as we don't need it
-        "debug.sf.nobootanimation=1",
     };
 
     for (const auto &prop : properties) {
