3e60d9f8aefbed0f2a4d45f8b9adfcaaffaa857d
Simon Fels <morphis@gravedo.de>
2017-01-15 14:13:10 +0100

    Don't set XDG_DATA_HOME to read-only location

--- scripts/snap-wrapper.sh
+++ scripts/snap-wrapper.sh
@@ -17,13 +17,11 @@ export LD_LIBRARY_PATH=$SNAP/usr/lib/$ARCH/mesa-egl:$LD_LIBRARY_PATH
 # XDG Config
 export XDG_CONFIG_DIRS=$SNAP/etc/xdg:$XDG_CONFIG_DIRS
 export XDG_CONFIG_DIRS=$SNAP/usr/xdg:$XDG_CONFIG_DIRS
+
 # Note: this doesn't seem to work, QML's LocalStorage either ignores
 # or fails to use $SNAP_USER_DATA if defined here
 export XDG_DATA_DIRS=$SNAP_USER_DATA:$XDG_DATA_DIRS
 export XDG_DATA_DIRS=$SNAP/usr/share:$XDG_DATA_DIRS
-
-# Not good, needed for fontconfig
-export XDG_DATA_HOME=$SNAP/usr/share
 
 # Tell libGL where to find the drivers
 export LIBGL_DRIVERS_PATH=$SNAP/usr/lib/$ARCH/dri
