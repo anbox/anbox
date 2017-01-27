4219f40ac37f359203bd94b450847b198d74213c
Simon Fels <morphis@gravedo.de>
2017-01-27 07:14:47 +0100

    Add missing copyright header for the cross-build scripts

--- cross-compile-chroot.sh
+++ cross-compile-chroot.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-# build script to compile anbox for armhf devices
 #
+# Copyright © 2016 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Taken from the Mir Project (https://launchpad.net/mir)
+
 set -e
 
 usage() {
--- scripts/setup-partial-armhf-chroot.sh
+++ scripts/setup-partial-armhf-chroot.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 #
-# TODO: Rename this file without "armhf" when it's safe to do so.
+# Copyright © 2016 Canonical Ltd.
 #
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Taken from the Mir Project (https://launchpad.net/mir)
 
 set -e
 
