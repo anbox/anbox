6bc7ee1b93edce0fd3279a22d59c02136c3d9591
Marius Gripsgard <mariogrip@ubuntu.com>
2018-01-04 14:36:10 +0100

    Add basic touchscreen support

--- src/anbox/platform/sdl/platform.h
+++ src/anbox/platform/sdl/platform.h
@@ -87,6 +87,8 @@ class Platform : public std::enable_shared_from_this<Platform>,
   bool event_thread_running_;
   std::shared_ptr<input::Device> pointer_;
   std::shared_ptr<input::Device> keyboard_;
+  std::shared_ptr<input::Device> touch_;
+  graphics::Rect display_frame_;
   bool window_size_immutable_ = false;
   bool single_window_ = false;
 };
