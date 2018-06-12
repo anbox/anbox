a7e2db21bca27df5b864e5ceee30aa6405caf92c
Simon Fels <morphis@gravedo.de>
2018-06-12 09:03:50 +0200

    scripts: also collect container console log files

--- scripts/collect-bug-info.sh
+++ scripts/collect-bug-info.sh
@@ -26,6 +26,7 @@ set -x
 cp /var/snap/anbox/common/data/system.log $TMPDIR || true
 cp /var/snap/anbox/common/containers/lxc-monitord.log $TMPDIR || true
 cp /var/snap/anbox/common/logs/container.log $TMPDIR || true
+cp /var/snap/anbox/common/logs/console.log* $TMPDIR || true
 $SNAP/command-anbox.wrapper system-info > $TMPDIR/system-info.log 2>&1 || true
 
 if [ -e /etc/systemd/system/snap.anbox.container-manager.service ]; then
