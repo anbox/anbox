4880a7b9dfbc91c48652873fe61081d781df640a
jnnksdev <jnnksdev@pm.me>
2020-09-06 18:31:49 +0200

    use current user and group id

--- scripts/build-with-docker.sh
+++ scripts/build-with-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t anbox/anbox-builder .
-docker run --rm -v $PWD:/anbox anbox/anbox-builder /anbox/scripts/clean-build.sh
+docker run --rm --user=$(id -u):$(id -g) --volume=$PWD:/anbox anbox/anbox-builder /anbox/scripts/clean-build.sh
