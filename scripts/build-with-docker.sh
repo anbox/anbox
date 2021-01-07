d461db5fe2e856c37a209fb9618c05efe031f3eb
jnnksdev <jnnksdev@pm.me>
2021-01-07 16:15:26 +0100

    use docker BuildKit

--- scripts/build-with-docker.sh
+++ scripts/build-with-docker.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-docker build -t anbox/anbox-builder .
+
+
+# DOCKER_BUILDKIT: alternative docker backend for slight performance improvement
+DOCKER_BUILDKIT=1 docker build -t anbox/anbox-builder .
 docker run --rm --user=$(id -u):$(id -g) --volume=$PWD:/anbox anbox/anbox-builder /anbox/scripts/build.sh $@
