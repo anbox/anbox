a2a67c33e1ceedf33561230d357c3a835ebfd20c
jnnksdev <jnnksdev@pm.me>
2021-01-22 13:08:15 +0100

    move build script to root

--- scripts/build-with-docker.sh
+++ scripts/build-with-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t anbox/anbox-builder .
-docker run --rm --user=$(id -u):$(id -g) --volume=$PWD:/anbox anbox/anbox-builder /anbox/scripts/build.sh $@
+docker run --rm --user=$(id -u):$(id -g) --volume=$PWD:/anbox anbox/anbox-builder /anbox/build.sh $@
